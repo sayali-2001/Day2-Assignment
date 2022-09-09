cd5e58484187b290e833afdda7fc10bfb11e313c
sayali-2001 <sayalithakare47@gmail.com>
2022-09-09 23:15:20 +0530

    Write a Program to Check Vowel or Consonant using Switch Case

---
+++ VowelConsonant.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+
+        char ch;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the Alphabet for check vowel or consonant  ");
+        ch=scan.next().charAt(0);
+
+        switch(ch){
+            case 'a':
+                System.out.println(ch+" is a vowel");
+                break;
+            case 'e':
+                System.out.println(ch+" is a vowel");
+                break;
+            case 'i':
+                System.out.println(ch+" is a vowel");
+                break;
+            case 'o':
+                System.out.println(ch+" is a vowel");
+                break;
+            case 'u':
+                System.out.println(ch+" is a vowel");
+                break;
+            case 'A':
+                System.out.println(ch+" is a vowel");
+                break;
+            case 'E':
+                System.out.println(ch+" is a vowel");
+                break;
+            case 'I':
+                System.out.println(ch+" is a vowel");
+                break;
+            case 'O':
+                System.out.println(ch+" is a vowel");
+                break;
+            case 'U':
+                System.out.println(ch+" is a vowel");
+                break;
+            default:
+                System.out.println(ch+" is a consonant");
+                break;
+        }
+    }
+}
