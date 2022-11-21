1b54d4001cb81d3d2604cdbf32cda395671b47a3
sayali-2001 <60540661+sayali-2001@users.noreply.github.com>
2022-11-21 20:35:36 +0530

    Read a Number 1,10,100,1000 and display unit, ten hundred

---
+++ PlaceValue.java
@@ -0,0 +1,25 @@
+package Day2;
+
+import java.util.Scanner;
+public class PlaceValue {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Available Lists ...");
+        System.out.print("1        10         100                  1000");
+        System.out.println("\nEnter Number From above Lists ");
+        int num= sc.nextInt();
+        if(num==1) {
+            System.out.println("You Enter UNIT");
+        } else if (num==10) {
+            System.out.println("You Enter TEN");
+        } else if (num==100) {
+            System.out.println("You Enter HUNDRED");
+        }
+        else if (num==1000) {
+            System.out.println("You Enter THOUSAND");
+        }
+        else {
+            System.out.println("Invalid Input.....");
+        }
+    }
+}
